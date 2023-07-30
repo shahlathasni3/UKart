6398b8b8bb7d6dc302f12f3fddebd05fbd60fcb9
Shahla Thasni <shahlathasni3@gmail.com>
2023-07-30 12:14:21 +0530

    handled cart and checkout page

--- accounts/views.py
+++ accounts/views.py
@@ -14,6 +14,9 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
 # Create your views here.
 
+from carts.views import _cart_id
+from carts.models import Cart,CartItem
+import requests
 
 def register(request):
     if request.method == 'POST':
@@ -58,9 +61,33 @@ def login(request):
         user = auth.authenticate(email=email,password=password)
 
         if user is not None:
+            try:
+                print("Entering inside try block")
+                cart = Cart.objects.get(cart_id=_cart_id(request))
+                is_cart_item_exists = CartItem.objects.filter(cart=cart).exists()
+                print(is_cart_item_exists)
+                if is_cart_item_exists:
+                    cart_item = CartItem.objects.filter(cart=cart)
+                    print(cart_item)
+
+                    for item in cart_item:
+                        item.user = user
+                        item.save()
+            except:
+                print("Entering inside except block")
+                pass
             auth.login(request, user)
             messages.success(request, "You are now logged in.")
-            return redirect('dashboard')
+            url = request.META.get('HTTP_REFERER')
+            try:
+                query = requests.utils.urlparse(url).query
+                
+                params = dict(x.split('=') for x in query.split('&'))
+                if 'next' in params:
+                    nextPage = params['next']
+                    return redirect(nextPage)
+            except:
+                return redirect('dashboard')
         else:
             messages.error(request, 'Invalid login credentials')
             return redirect('login')
